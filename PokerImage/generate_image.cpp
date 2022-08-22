0835d343665ddc996ead2d3ca354de7de409e75d
夏昌阔 <330018177@qq.com>
2022-08-22 23:37:17 +0800

    ✨ feat(generate): 增加判断传入参数

--- PokerImage/generate_image.cpp
+++ PokerImage/generate_image.cpp
@@ -1,5 +1,29 @@
 #include "generate_image.h"
 
+#include <future>
+#include <json_helper/jsoncpp_helper.hpp>
+
 int GenerateImage(const std::string& req, OnCallbackFunc on_callback) {
-  return 0;
+  int result = -1;
+  do {
+    Json::Value root;
+    if (!on_callback || !jsoncpp_helper::ReaderHelper(req, root)) {
+      break;
+    }
+    const auto& output = jsoncpp_helper::GetString(root, "output");
+    // 输出路径不能为空
+    if (output.empty() || output.back() != '/') {
+      break;
+    }
+    Json::Value& cards = root["cards"];
+    if (cards.type() != Json::arrayValue) {
+      break;
+    }
+    const auto& size = cards.size();
+    std::async(std::launch::async, [cards, size, output, on_callback](){
+      
+    });
+    result = 0;
+  } while (false);
+  return result;
 }
